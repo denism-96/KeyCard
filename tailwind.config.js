366c77e5011da6c0c333431c3541a84e2327c4ba
denism96 <drag33546@bk.ru>
2026-05-20 23:44:31 +0300

    chore(config): install PostCss,TW,prettier config

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./client/**/*.{html,js,jsx,ts,tsx}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
