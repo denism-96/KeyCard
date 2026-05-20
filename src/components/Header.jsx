83becceeb859d779caeea960491e342cc8e6db60
denism96 <drag33546@bk.ru>
2026-05-20 21:13:11 +0300

    feat(layout): add header and footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,20 @@
+import { NavLink } from "react-router";
+import Button from "./Ui/Button";
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div>
+        <img src="#" alt="main logo" />
+        <nav>
+          <NavLink to="/">О нас</NavLink>
+          <NavLink to="/">Услуги</NavLink>
+          <NavLink to="/">Кейсы</NavLink>
+          <NavLink to="/">Тарифы</NavLink>
+          <NavLink to="/">Статьи</NavLink>
+          <NavLink to="/">Контакты</NavLink>
+        </nav>
+        <Button>Получить консультацию</Button>
+      </div>
+    </header>
+  );
 }
