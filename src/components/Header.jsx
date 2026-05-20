846b27c63b33e1c9e2e1b0b35c3068814fbbf44a
denism96 <drag33546@bk.ru>
2026-05-20 23:45:45 +0300

    style(nav): add hover animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,27 @@ import Button from "./Ui/Button";
 export default function Header() {
   return (
     <header>
-      <div>
+      <div className="flex items-center justify-between">
         <img src="#" alt="main logo" />
-        <nav>
-          <NavLink to="/">О нас</NavLink>
-          <NavLink to="/">Услуги</NavLink>
-          <NavLink to="/">Кейсы</NavLink>
-          <NavLink to="/">Тарифы</NavLink>
-          <NavLink to="/">Статьи</NavLink>
-          <NavLink to="/">Контакты</NavLink>
+        <nav className="header-nav flex items-center gap-8 text-sm font-medium text-(--text-gray)">
+          <NavLink to="/" className="header-link">
+            О нас
+          </NavLink>
+          <NavLink to="/" className="header-link">
+            Услуги
+          </NavLink>
+          <NavLink to="/" className="header-link">
+            Кейсы
+          </NavLink>
+          <NavLink to="/" className="header-link">
+            Тарифы
+          </NavLink>
+          <NavLink to="/" className="header-link">
+            Статьи
+          </NavLink>
+          <NavLink to="/" className="header-link">
+            Контакты
+          </NavLink>
         </nav>
         <Button>Получить консультацию</Button>
       </div>
